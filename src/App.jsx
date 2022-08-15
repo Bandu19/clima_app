0e4228f2179ed02581748b0c9f4e406692841496
Oscar Acuña <oscagustavo1961@gmail.com>
2022-08-15 15:04:36 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { AppClima } from "./components";
+
+function App() {
+   return (
+      <>
+         <h1>INICIO</h1>
+         <AppClima />
+      </>
+   );
+}
+
+export default App;
