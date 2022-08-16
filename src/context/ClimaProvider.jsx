85d2495b7ee47092292235fd8d8ff74df91e2108
Oscar Acuña <oscagustavo1961@gmail.com>
2022-08-16 10:27:39 -0500

    Cosultando la APIclima y APIlocation

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -1,14 +1,17 @@
 import { useState, createContext } from "react";
-
+import axios from "axios";
 const ClimaContext = createContext();
 
 export const ClimaProvider = ({ children }) => {
-   console.log(import.meta.env.VITE_API_KEY);
+   // console.log(import.meta.env.VITE_API_KEY);
 
    const [busqueda, setBusqueda] = useState({
       ciudad: "",
       pais: "",
    });
+
+   // Espera recibir un objeto por respuesta
+   const [resultado, setResultado] = useState({});
 
    const datosBusqueda = (e) => {
       setBusqueda({
@@ -20,8 +23,37 @@ export const ClimaProvider = ({ children }) => {
    };
 
    // Imprime en consola los datos agregados en el Formulario
-   const consultarClima = (datos) => {
-      console.log(datos);
+   // IMPORTANT
+   const consultarClima = async (datos) => {
+      try {
+         //extraer ciudad o pais
+         const { ciudad, pais } = datos;
+
+         //Esta es nuestra llave de la API
+         const appId = import.meta.env.VITE_API_KEY;
+
+         //==> Si funciona let clima = navigator.geolocation;
+         // console.log(clima);
+
+         // LLamamos la API -->>
+         const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`;
+
+         // LLAMAMOS AXIOS
+         const { data } = await axios(url);
+         // console.log(data[0]);
+         const { lat, lon } = data[0];
+
+         const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+
+         const { data: clima } = await axios(urlClima);
+         // ENVIA RESPUESTA POR OBJETO
+         console.log(clima);
+         setResultado(clima);
+
+         // console.log(url);
+      } catch (error) {
+         console.log(datos);
+      }
    };
 
    return (
@@ -31,6 +63,7 @@ export const ClimaProvider = ({ children }) => {
             busqueda,
             datosBusqueda,
             consultarClima,
+            resultado,
          }}
       >
          {children}
